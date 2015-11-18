3cd68df8636a419d6dfd79676c05a041d87cbab0
kyle macpherson <kaimac2010@gmail.com>
2015-11-18 18:19:42 +0000

    added song playing function

--- www/js/playercontroller.js
+++ www/js/playercontroller.js
@@ -3,6 +3,8 @@ ravebot.controller('PlayerCtrl', ['$scope', '$http', '$state', '$location', '$co
   $scope.currentSong = ''
   $scope.songMessage = ''
 
+  $scope.songPlaying = false
+
   $scope.trustSrc = function(src) {
     return $sce.trustAsResourceUrl(src);
   };
@@ -11,10 +13,22 @@ ravebot.controller('PlayerCtrl', ['$scope', '$http', '$state', '$location', '$co
     $http.get('https://stormy-bastion-7671.herokuapp.com/songs/random/' + $scope.partyName, 'GET').success(function(response) {
       $scope.currentSong = 'https://w.soundcloud.com/player/?url=' + response.url + '&auto_play=true'
       $scope.songMessage = response.message
+      $scope.songPlaying(response)
       setTimeout(function () {
         $state.reload()
       }, response.duration);
     });
+  }
+
+  $scope.songPlaying = function(response) {
+    if(response.url == null) {
+      $scope.songPlaying = false
+      setTimeout(function () {
+      $state.reload()
+      }, 5000);
+    } else {
+      $scope.songPlaying = true
+    }
   }
 
 }]);
